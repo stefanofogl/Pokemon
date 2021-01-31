1ec605e0e2011b456106c1ee4349d04c0dad6682
Stefano Foglia <stefano.foglia@si2001.it>
2021-02-01 00:35:26 +0100

    Initial Commit

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Stefano Foglia on 01/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
